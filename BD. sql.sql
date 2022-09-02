2b6803878858d9af7999b524eb767659bf1d5833
Davi Xavier Buzato <davixbuzato@gmail.com>
2022-09-02 14:29:34 -0300

    Implementei o Banco

---
+++ BD. sql.sql
@@ -0,0 +1,27 @@
+use master
+go
+
+if(exists(select * from sys.databases where name =  'TCC_CODEX'))
+	drop database TCC_CODEX
+go
+
+create database TCC_CODEX
+go
+
+use TCC_CODEX
+go
+
+create table Usuario(
+	ID int identity primary key,
+	Nome varchar(200) not null,
+	Senha varchar(1000) not null,
+);
+go
+
+create table Post(
+	ID int identity primary key,
+	Publicante varchar(200) not null,
+	Conteudo varchar (MAX) not null,
+	Momento datetime not null
+);
+go
